1144f37052fd9ec34078cddaa6a68c459cb7cc92
FrankBonanno <fb0023@mix.wvu.edu>
2023-04-20 10:50:49 -0400

    customapi error and db connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const connectDB = require('./db/connect');
 
 const app = express();
 const PORT = process.env.PORT;
 
+/* APP CONFIG */
+app.use(morgan('dev'));
+
+/* MIDDLEWARE */
+app.use(express.json());
+
+/* ROUTES */
+app.get('/', (req, res) => {
+    res.send('Server Working!');
+});
+
+/* ERROR HANDLING */
+app.use(notFound);
+app.use(errorHandler);
+
 /* Request Listener */
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(PORT, () => {
             console.log(`Server Listening On Port ${PORT}...`);
         });
