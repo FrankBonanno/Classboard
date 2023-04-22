6a40c101975610a34243e810f7c677e4f1fecdec
FrankBonanno <fb0023@mix.wvu.edu>
2023-04-22 17:21:24 -0400

    Courses model

---
+++ server/models/Course.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const AssignmentSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please Provide The Assignment Name'],
+            trim: true,
+            unique: true,
+        },
+        dueDate: {
+            type: Date,
+            default: new Date(),
+            required: [true, 'Please Provide The Due Date'],
+        },
+        completed: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const CourseSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please Provide The Course Name'],
+        trim: true,
+        unique: true,
+    },
+    color: {
+        type: String,
+        required: [true, 'Please Provide A Course Color'],
+    },
+    assignments: {
+        type: [AssignmentSchema],
+        default: [],
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please Provide User'],
+    },
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
