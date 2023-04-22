63533db048af7b3268933c95c4426856038f20ae
FrankBonanno <fb0023@mix.wvu.edu>
2023-04-22 17:06:21 -0400

    course router and controller init

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
 const connectDB = require('./db/connect');
 const notFound = require('./middleware/notFound');
 const errorHandler = require('./middleware/errorHandler');
+const coursesRouter = require('./routes/courses');
 
 const app = express();
 const PORT = process.env.PORT;
 
 /* APP CONFIG */
+app.use(cors());
+app.use(helmet());
 app.use(morgan('dev'));
 
 /* MIDDLEWARE */
 app.use(express.json());
 
 /* ROUTES */
-app.get('/api', (req, res) => {
-    res.send('Server Working!');
-});
+app.use('/api/classes', coursesRouter);
 
 /* ERROR HANDLING */
 app.use(notFound);
