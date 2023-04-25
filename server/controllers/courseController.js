83852f178e427b84fc39e8e67da46fa4ddde0fab
FrankBonanno <fb0023@mix.wvu.edu>
2023-04-25 11:46:13 -0400

    course controller started

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -1,3 +1,5 @@
+const { StatusCodes } = require('http-status-codes');
+const { NotFoundError } = require('../errors');
 const Course = require('../models/Course');
 
 const getCourses = async (req, res) => {
@@ -6,4 +8,30 @@ const getCourses = async (req, res) => {
     res.status(200).json({ count: courses.length, courses });
 };
 
-module.exports = { getCourses };
+const getCourse = async (req, res) => {
+    const userId = req.user.userId;
+    const courseId = req.params.id;
+    const course = await Course.findOne({ _id: courseId, createdBy: userId });
+    if (!course) {
+        throw new NotFoundError(`No Course Found With ID: ${courseId}`);
+    }
+    res.status(StatusCodes.OK).json({ course });
+};
+
+const createCourse = async (req, res) => {
+    req.body.createdBy = req.user.userId;
+    const course = await Course.create(req.body);
+    res.status(StatusCodes.CREATED).json({ course });
+};
+
+const deleteCourse = async (req, res) => {
+    const userId = req.user.userId;
+    const courseId = req.params.id;
+    const deletedCourse = await Course.findOneAndDelete({ _id: courseId, createdBy: userId });
+    if (!deletedCourse) {
+        throw new NotFoundError(`No Course Found With ID: ${courseId}`);
+    }
+    res.status(StatusCodes.OK).json({ deletedCourse });
+};
+
+module.exports = { getCourses, getCourse, createCourse, deleteCourse };
